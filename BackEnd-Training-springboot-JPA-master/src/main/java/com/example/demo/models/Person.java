03c8d81f6437433643f5919538c199f774911358
Leena Bany Odeh <lena.oudeh@gmail.com>
2021-07-28 23:22:08 +0300

    first version

--- BackEnd-Training-springboot-JPA-master/src/main/java/com/example/demo/models/Person.java
+++ BackEnd-Training-springboot-JPA-master/src/main/java/com/example/demo/models/Person.java
@@ -15,156 +15,36 @@ import java.util.Set;
 @Table(name = "person")
 @Setter
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 //@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 //@MappedSuperclass
-public  class Person   {
+public  class Person {
     //private static final long serialVersionUID = 1L;
 
     @Id
     //@GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name="id")
+    @Column(name = "id")
     private int id;
-    @Column(nullable = false,unique = true)
+    @Column(nullable = false, unique = true)
 
     @NotEmpty(message = "must fill name field")
-    @Size(min =3,max = 15,message = "name must be at least 3 character and not exceed 15")
-    private  String name;
-//    @OneToMany(mappedBy = "person",cascade = CascadeType.ALL)
-//    private Set<PhoneNumber> numbers;
+    @Size(min = 3, max = 15, message = "name must be at least 3 character and not exceed 15")
+    private String name;
+
+
+    @OneToMany(mappedBy = "person", cascade = CascadeType.ALL)
+    private Set<PhoneNumber> numbers;
+
     private String address;
     @Temporal(TemporalType.DATE)
     private Date dob;
 
-
-    //@OneToOne(mappedBy = "person",cascade = CascadeType.ALL)
-
-    //@OneToOne(cascade = CascadeType.ALL)
-    //@JoinColumn(name="ACCOUNT_ID")
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "account_id", referencedColumnName = "id")
     private BankAccount bankAccount;
 
-//
 
 
-    public Person(int id, String name, String address, Date dob, BankAccount bankAccount) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.dob = dob;
-        this.bankAccount = bankAccount;
-    }
-
-    public Person() {
-
-    }
-
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-
-//    public BankAccount getBankAccount() {
-//        return bankAccount;
-//    }
-//
-//    public void setBankAccount(BankAccount bankAccount) {
-//        this.bankAccount = bankAccount;
-//    }
-
-//    public String getAddress() {
-//        return address;
-//    }
-//
-//    public void setAddress(String address) {
-//        this.address = address;
-//    }
-////
-//    public Date getDob() {
-//        return dob;
-//    }
-//
-//    public void setDob(Date dob) {
-//        this.dob = dob;
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-
-
-//    public Set<PhoneNumber> getNumbers() {
-//        return numbers;
-//    }
-
-//    @Override
-//    public String toString() {
-//        return "Person{" +
-//                "id=" + id +
-//                ", name='" + name + '\'' +
-//                ", numbers=" + numbers +
-//                ", address='" + address + '\'' +
-//                ", dob=" + dob +
-//                ", bankAccount=" + bankAccount +
-//                '}';
-//    }
-
-//    public void addPhoneNumber(PhoneNumber number){
-//        if(number !=null){
-//            if(numbers==null){
-//                numbers=new HashSet<>();
-//            }
-//            number.setPerson(this);
-//            numbers.add(number);
-//        }
-//    }
-
-//    public static class Builder {
-//        private int id;
-//        private  String name;
-//        private Set<PhoneNumber> numbers;
-//        private BankAccount bankAccount;
-//        private String address;
-//        @Temporal(TemporalType.DATE)
-//        private Date dob;
-//
-//
-//
-//        public Builder setId(int id) {
-//            this.id = id;
-//            return this;
-//        }
-//
-//        public Builder setName(String name) {
-//            this.name = name;
-//            return this;
-//        }
-//
-//
-//
-//        public Builder setAddress(String address) {
-//            this.address = address;
-//            return this;
-//        }
-//
-//        public Builder setDob(Date dob) {
-//            this.dob = dob;
-//            return this;
-//        }
-//
-//
-//
-//        public Person build(){
-//
-//            return new Person(id,name, address,dob);
-//        }
-//    }
 }
--- BackEnd-Training-springboot-JPA-master/src/main/java/com/example/demo/models/PhoneNumber.java
+++ BackEnd-Training-springboot-JPA-master/src/main/java/com/example/demo/models/PhoneNumber.java
@@ -11,17 +11,21 @@ public class PhoneNumber {
     private String number;
     private String type;
 
-//    public Person getPerson() {
-//        return person;
-//    }
-//
-//    public void setPerson(Person person) {
-//        this.person = person;
-//    }
 
-//    @ManyToOne
-//    @JoinColumn(name="person_id")
-//    private Person person;
+
+    @ManyToOne
+    @JoinColumn(name="phone_id", nullable=false)
+    private Person person;
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+
 
     public long getPid() {
         return pid;
