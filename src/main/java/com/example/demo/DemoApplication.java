34f34c199161d0da3df24b9d9e593e2fadff6326
Leena Bany Odeh <lena.oudeh@gmail.com>
2021-08-14 13:55:39 +0300

    MongoDb

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,19 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
 
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("MongoDB Branch");
 
 		SpringApplication.run(DemoApplication.class, args);// CREATE SPRING CONTAINER.
 
